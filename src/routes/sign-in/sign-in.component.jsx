b356a9987f5efe3662034a703b59b602931ef217
ward-qaruty <ward.muse.dev@gmai.com>
2022-04-06 02:12:40 +0300

    sign in using firebase

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+import {
+  singInWithGooglePopup,
+  createUserDocFromAuth,
+} from "../../utils/firebase/firebase.utils";
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await singInWithGooglePopup();
+    const userDocRef = await createUserDocFromAuth(user);
+  };
+
   return (
     <div>
-      <h1>Sing In page</h1>
+      <h1>Sign In page</h1>
+      <button onClick={logGoogleUser}>Sing in with Google</button>
     </div>
   );
 };

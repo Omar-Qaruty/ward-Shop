848448e74835927e3c8f710c5c6943fb95de411b
Omar Qaruty <ward.muse.dev@gmail.com>
2022-05-18 17:40:42 +0300

    Memoize categorySelector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,6 +1,23 @@
-export const selectCategoriesMap = (state) =>
-  state.categories.categories.reduce((acc, category) => {
-    const { title, items } = category;
-    acc[title.toLowerCase()] = items;
-    return acc;
-  }, {});
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => state.categories;
+
+// Memoization
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+// As long as the categories array does not change, don't run the "reduce method".
+// after the first run, it will give back the previous value as long its not changing
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
